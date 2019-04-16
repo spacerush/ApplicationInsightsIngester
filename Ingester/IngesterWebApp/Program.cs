9f8ba9ca532dad1e535fd360e698bb9f904fc063
Philip <philip@spacerush.app>
2019-04-16 01:04:35 -0700

    Testing.

---
+++ Ingester/IngesterWebApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.ApplicationInsights;
+using Microsoft.Diagnostics.EventFlow.HealthReporters;
+using Microsoft.Diagnostics.EventFlow.Inputs;
+using Microsoft.Diagnostics.EventFlow.Outputs;
+using Microsoft.Diagnostics.EventFlow.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.AspNetCore;
+using IngesterWebApp;
+
+namespace AspNetCoreEventFlow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var eventFlow = CreateEventFlow(args))
+            {
+                BuildWebHost(args, eventFlow).Run();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args, DiagnosticPipeline eventFlow) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddSingleton<ITelemetryProcessorFactory>(sp => new EventFlowTelemetryProcessorFactory(eventFlow)))
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+        private static DiagnosticPipeline CreateEventFlow(string[] args)
+        {
+            // Create configuration instance to access configuration information for EventFlow pipeline
+            // To learn about common configuration sources take a peek at https://github.com/aspnet/MetaPackages/blob/master/src/Microsoft.AspNetCore/WebHost.cs (CreateDefaultBuilder method). 
+            // Here we assume all necessary information comes from command-line arguments and environment variables.
+            var configBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            if (args != null)
+            {
+                configBuilder.AddCommandLine(args);
+            }
+            var config = configBuilder.Build();
+
+            var healthReporter = new CsvHealthReporter(new CsvHealthReporterConfiguration());
+            var aiInput = new ApplicationInsightsInputFactory().CreateItem(null, healthReporter);
+            var inputs = new IObservable<EventData>[] { aiInput };
+            var sinks = new EventSink[]
+            {
+                new EventSink(new StdOutput(healthReporter), null)
+            };
+
+            return new DiagnosticPipeline(healthReporter, inputs, null, sinks, null, disposeDependencies: true);
+        }
+    }
+}
---
+++ Ingester/IngesterWebApp/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngesterWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var client = new MongoClient("mongodb://localhost:27017/Ingester");
+            services.AddSingleton<IMongoClient>(c => client);
+            IServiceProvider provider = services.BuildServiceProvider();
+            IMongoClient mongo = provider.GetRequiredService<IMongoClient>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            }
+        }
+    }
+}
