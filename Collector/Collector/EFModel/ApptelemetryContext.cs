f54e47a4fd756b25e321662e8cdbd4d16d12a636
Philip <philip@spacerush.app>
2019-04-20 00:05:25 -0700

    add EF core and db context and model.

---
+++ Collector/Collector/EFModel/ApptelemetryContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Collector.EFModel
+{
+    public partial class ApptelemetryContext : DbContext
+    {
+        public ApptelemetryContext()
+        {
+        }
+
+        public ApptelemetryContext(DbContextOptions<ApptelemetryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TelemetryLog> TelemetryLogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Collector/Collector/EFModel/TelemetryLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collector.EFModel
+{
+    public partial class TelemetryLog
+    {
+        public long TelemetryLogId { get; set; }
+        public string TelemetryData { get; set; }
+    }
+}
